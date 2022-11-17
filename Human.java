bb482b2398c909309eebd22fdb19a80304eae148
oxaoz <ogozer@ya.ru>
2022-11-18 04:12:57 +0700

    Задание 2

--- Human.java
+++ Human.java
@@ -4,18 +4,59 @@ public class Human {
     String name;
     String town;
     String jobTitle;
+    String zero = "«Информация не указана»";
+
+
 
     public Human(String name, String town, int yearOfBirth, String jobTitle) {
         this.name = name;
         this.town = town;
         this.yearOfBirth = yearOfBirth;
         this.jobTitle = jobTitle;
-    }
 
+        if (yearOfBirth > 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else this.yearOfBirth = 0;
+
+        if (name == null || name.isEmpty()) {
+            this.name = zero;
+        } else this.name = name;
+
+        if (town == null || town.isEmpty()) {
+            this.town = zero;
+        }else this.town = town;
+
+        if (jobTitle == null || jobTitle.isEmpty()) {
+            this.jobTitle = zero;
+        }else this.jobTitle = jobTitle;
+        }
+
+
+    @Override
     public String toString() {
         return "Привет! Меня зовут " + name
                 + ". Я из города " + town +
                 ". Я родился " + yearOfBirth + " году." +
                 "Я работаю на должности " + jobTitle + " Будем знакомы!";
     }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj.getClass().equals(this.getClass())) {
+            if (obj == this) return true;
+            Human human = (Human) obj;
+            return name.equals(((Human) obj).name) && yearOfBirth == ((Human) obj).yearOfBirth && town.equals(((Human) obj).town) && jobTitle.equals(((Human) obj).jobTitle);
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name, yearOfBirth, town, jobTitle);
+    }
 }
+/*Если не указано имя, город проживания или должность, то есть передана пустая строка или null,
+то программа должна подставить значение «Информация не указана» при выводе в консоль.
+ */
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args){
-        Human maxim = new Human("Максим", "Минск", 1988, "бренд-менеджера");
+        Human maxim = new Human("", "Минск", -1988, "бренд-менеджера");
         Human anya = new Human("Аня", "Москва", 1993, "методиста образовательных программ");
         Human katya = new Human("Катя", "Калининград", 1992, "продакт-менеджера");
         Human artem = new Human("Артём", "Москва", 1995, "директора по развитию бизнеса");
